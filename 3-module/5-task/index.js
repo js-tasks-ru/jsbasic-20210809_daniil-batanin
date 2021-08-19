95d87ffbc08b50af80daeedf50979e1d59f58e0e
Daniil Batanin <pikabu1305@gmail.com>
2021-08-19 20:46:14 +0300

    Completed third module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,4 @@
 function getMinMax(str) {
-  // ваш код...
+  newStr = str.split(" ").filter(item => !isNaN(item)).sort((a, b) => a - b);
+  return {min: +newStr[0], max: +newStr[newStr.length - 1]}
 }
