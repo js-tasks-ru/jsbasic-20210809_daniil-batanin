79a296dbc997fffded98ce9a29ff21967e384d09
Daniil Batanin <pikabu1305@gmail.com>
2021-08-27 00:48:59 +0300

    Completed fifth module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  const divText = document.querySelector('.toggle-text-button');
+  text = document.querySelector('#text');
+  divText.addEventListener('click', () => {
+    if (text.hidden) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  });
+  
+  
+
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  const slide = document.querySelector('.carousel__inner');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+
+  let positionSlide = 0;
+  arrowLeft.style.display = 'none';
+
+  function checkArrow() {
+    if (positionSlide == 0) {
+      arrowLeft.style.display = 'none';
+    } else {
+      arrowLeft.style.display = '';
+    }
+    if (positionSlide == (-3 * slide.offsetWidth)) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = '';
+    }
+  }
+
+  arrowRight.addEventListener('click', () => {
+    slide.style.transform = `translateX(${positionSlide -= slide.offsetWidth}px)`;
+    checkArrow();
+  });
+  arrowLeft.addEventListener('click', () => {
+    slide.style.transform = `translateX(${positionSlide += slide.offsetWidth}px)`;
+    checkArrow();
+  });
+
+  
+
+  
 }
